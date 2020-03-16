053bbf001f44712f904674e7326e794f3c055b7e
alceil <ashishshajisrampickal@gmail.com>
2020-03-16 12:19:00 +0530

    added db rules

--- lib/database.dart
+++ lib/database.dart
@@ -1,4 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 class DataBaseServices{
+  final String uid;
+  DataBaseServices({this.uid});
   final CollectionReference brewcollections=Firestore.instance.collection("brews");
+  Future updateUserData(String sugar,String name,int strength) async
+  {
+    return await brewcollections.document(uid).setData({
+      'sugars':sugar,
+      'name':name,
+      'strength':strength
+
+    });
+  }
 }
